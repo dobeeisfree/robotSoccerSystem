aa96d70aa5b12b0b21153fff233e41c43ce1671c
Hwang Ju Bee <wnql301@naver.com>
2017-05-08 02:48:06 +0900

    main 을 클래스로 리팩토링
    비전시스템 클래스를 둠
    그리고 혜준이도 코딩할 수있게 주석도 영어로 담

---
+++ mobileSoccer/main.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "robot.h"
+#include "gui_text.h"
+#include <opencv2/core/mat.hpp>  
+#include <opencv2/imgcodecs.hpp>  
+#include <opencv2/imgproc.hpp>  
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#define CAP_NUMBER 0	// Set WebCam Number
+
+using namespace cv;
+using namespace std;
+
+VideoCapture cap(CAP_NUMBER);				// define WebCam
+robot team, id1, id2 = robot();			// robot colors
+
+class VisionSystem
+{
+public:
+	VisionSystem();
+	~VisionSystem();
+	void SetVideoSize(int width, int height);
+	void GenerateTrackbar(char * msg, robot * color, char * TrackbarName);
+	void SetUI(char * msg, robot * teamColor, robot * id1Color, robot * id2Color);
+	void SetRobotColors();
+	void drawAreaBox(Mat img_input, Mat stats, int numOfLables);
+	void VisionStart();
+};
