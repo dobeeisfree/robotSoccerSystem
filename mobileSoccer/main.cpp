ced4ed3f1ca4153cb0515441f4d485cd560a18d9
Hwang Ju Bee <wnql301@naver.com>
2017-05-10 21:02:25 +0900

    코드합작
    혜준의 방정식과 함수를 통해 우리는 직선의 방정식까지 했다. 그리고 방향만 도출하면된다. 그리고 콘솔 유아이도 바꾸기로 했는데 일단 `system("cls")` 로 했다.

--- mobileSoccer/main.cpp
+++ mobileSoccer/main.cpp
@@ -4,7 +4,6 @@ VisionSystem::VisionSystem()
 {
 }
 
-
 VisionSystem::~VisionSystem()
 {
 }
@@ -89,26 +88,86 @@ void VisionSystem::drawAreaBox(Mat img_input, Mat stats, int numOfLables, char *
 	int top = stats.at<int>(idx, CC_STAT_TOP);
 	int width = stats.at<int>(idx, CC_STAT_WIDTH);
 	int height = stats.at<int>(idx, CC_STAT_HEIGHT);
-	
-	// TODO
-	// Calculate CenterPoint x,y
-	double Cx = (left + width) / 2;
-	double Cy = (top + height) / 2;
-	printf("[%s] CenterPoint (%.2lf, %.2lf) \n", title, Cx, Cy);
-	printf("[%s] left: %d, top: %d, width: %d, height: %d\n", title, left, top, width, height);
-	calculateTheLine(Cx, Cy, left, top, width, height);
+	printf("[%s] (%d, %d, %d, %d)\n", title, left, top, width, height);
+
+	if (strcmp(title, "id1") == 0)
+	{
+		x_one = round((double)(left + (double)(width / 2)));
+		y_one = round((double)(top + (double)(height / 2)));
+		printf("[%s] round:(%lf, %lf)\n", title, x_one, y_one);
+	}
+	else if (strcmp(title, "id2") == 0)
+	{
+		x_two = round((double)(left + (double)(width / 2)));
+		y_two = round((double)(top + (double)(height / 2)));
+		printf("[%s] round:(%lf, %lf)\n", title, x_two, y_two);
+	}
+
 	rectangle(img_input, Point(left, top), Point(left + width, top + height), Scalar(0, 0, 255), 1);
 }
 
-void VisionSystem::calculateTheLine(double Cx, double Cy, int left, int top, int width, int height)
+void VisionSystem::makeLine(double x1, double y1, double x2, double y2)
 {
+	// 직선의 방정식 기본 형태 :  y = ax + b
+	// 두점이 (x1, y1), (x2, y2)라고 할때 기울기 a는 다음과 같다.
+	// a = (y2 - y1) / (x2 - x1)
+	a = double(y2 - y1) / (x2 - x1);
 
+	// y = ax + b 이고 ax를 넘기면 아래와 같은 공식이 성립한다.
+	// b = y - ax;
+	b = y2 - (a*x2);
+}
+double VisionSystem::returnY(double x)
+{
+	return a*x + b;
+}
+
+void VisionSystem::findCenterPoint(double x1, double y1, double x2, double y2)
+{
+	Cx = (x1 + x2) / 2;
+	Cy = (y1 + y2) / 2; //find robot's center point
+}
+
+void VisionSystem::makeTheta(double x1, double y1, double x2, double y2)
+{
+	theta = atan(tan((y1 - y2) / (x1 - x2) - (3.1415f / 4)));
+}
+
+void VisionSystem::rtnRobotsDirection(double x)
+{
+	//회전식: 
+	Rx = (x - Cx)*cos(theta) - (returnY(x) - Cy)*sin(theta);
+	Ry = (returnY(x) - Cy)*cos(theta) + (x - Cx)*sin(theta);
+	//좌표의 형태 : (x, returnY(x))
+	cout << "두 점 사이를 이은 식: " << "y =" << a << "x+" << b << endl;
+
+	a2 = double(Ry - Cy) / (Rx - Cx);
+	b2 = Ry - a2*Rx;
+
+	cout << "로봇의 방향을 가리키는 직선: " << "y =" << a2 << "x+" << b2 << endl;
+}
+
+
+void VisionSystem::calculateTheLine(double x1, double y1, double x2, double y2)
+{
+	printf("each CenterPoint : (%lf, %lf), (%lf, %lf)\n", x1, y1, x2, y2);
+
+	makeLine(x1, y1, x2, y2);
+	findCenterPoint(x1, y1, x2, y2);
+	makeTheta(x1, y1, x2, y2);
+	//for (int i = 0; i < 100; i++)
+	//{
+	rtnRobotsDirection(x1);
+	//}
 }
 
 void VisionSystem::VisionStart()
 {
 	while (true)
 	{
+		// 생각해보자 콘솔유아이
+		system("cls");
+
 		// print Webcam velocity  
 		int fps = cap.get(CAP_PROP_FPS);
 		cout << "Webcam fps:" << fps << endl;
@@ -167,6 +226,7 @@ void VisionSystem::VisionStart()
 		drawAreaBox(img_input, team_stats, team.numOfLables, "team");
 		drawAreaBox(img_input, id1_stats, id1.numOfLables, "id1");
 		drawAreaBox(img_input, id2_stats, id2.numOfLables, "id2");
+		calculateTheLine(x_one, y_one, x_two, y_two);
 
 		//imshow("binary image", team.img_binary);
 		imshow("Origin IMAGE", img_input);
--- mobileSoccer/main.h
+++ mobileSoccer/main.h
@@ -5,6 +5,7 @@
 #include <opencv2/imgcodecs.hpp>  
 #include <opencv2/imgproc.hpp>  
 #include <opencv2/highgui.hpp>
+#include <math.h>
 #include <iostream>
 #define CAP_NUMBER 0	// Set WebCam Number
 
@@ -14,9 +15,20 @@ using namespace std;
 VideoCapture cap(CAP_NUMBER);				// define WebCam
 robot team, id1, id2 = robot();			// robot colors
 
+double a, b;
+double Cx, Cy;
+double theta;
+double a2, b2;
+double Rx, Ry;
+// id1, id2
+double x_one, x_two;
+double y_one, y_two;
+
+
 class VisionSystem
 {
 public:
+
 	VisionSystem();
 	~VisionSystem();
 	void SetVideoSize(int width, int height);
@@ -25,5 +37,10 @@ public:
 	void SetRobotColors();
 	void drawAreaBox(Mat img_input, Mat stats, int numOfLables, char * title);
 	void VisionStart();
-	void calculateTheLine(double Cx, double Cy, int left, int top, int width, int height);
+	void calculateTheLine(double x1, double y1, double x2, double y2);
+	void makeLine(double x1, double y1, double x2, double y2);
+	double returnY(double x);
+	void findCenterPoint(double x1, double y1, double x2, double y2);
+	void makeTheta(double x1, double y1, double x2, double y2);
+	void rtnRobotsDirection(double x);
 };
