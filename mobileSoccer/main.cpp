22cc482eaf3f9d5b5fbc6ff445053426631becd1
dobeeisfree <wnql301@naver.com>
2017-06-18 16:57:50 +0900

    로봇관련 함수 추가
    from 혜 robot.cpp

--- mobileSoccer/main.cpp
+++ mobileSoccer/main.cpp
@@ -137,6 +137,139 @@ int VisionSystem::angle(float x1, float y1, float x2, float y2)
 	else return 360 - angle - 45;
 }
 
+void VisionSystem::Velocity(Robot *robot, int vl, int vr)
+{
+	//Put your velocity-data-transfer between vision and arduino codes in here
+	//아두이노로 신호 전송하는 코드.
+}
+
+void VisionSystem::RobotAngle(Robot *robot, int desired_angle)
+{
+	int theta_e, vl, vr;
+	theta_e = desired_angle - (int)robot->_angle;
+
+	while (theta_e > 180) theta_e -= 360;
+	while (theta_e < -180) theta_e += 360;
+
+	if (theta_e < -90) theta_e += 180;
+
+	else if (theta_e > 90) theta_e -= 180;
+
+	if (abs(theta_e) > 50)
+	{
+		vl = (int)(-9. / 90.0 * (double)theta_e);
+		vr = (int)(9. / 90.0 * (double)theta_e);
+	}
+	else if (abs(theta_e) > 20)
+	{
+		vl = (int)(-11.0 / 90.0 * (double)theta_e);
+		vr = (int)(11.0 / 90.0 * (double)theta_e);
+	}
+	else
+	{
+		vl = (int)(-13.0 / 90.0 * (double)theta_e);
+		vr = (int)(13.0 / 90.0 * (double)theta_e);
+	}
+	Velocity(robot, vl, vr);//계산된 회전해야하는 앵글값대로 바퀴조정 ->아두이노로 보내는 함수 호출!
+}
+
+void VisionSystem::Position(Robot *robot, double x, double y)
+{
+	int desired_angle = 0, theta_e = 0, d_angle = 0, vl, vr, vc = 125;
+
+	double dx, dy, d_e, Ka = 10.0 / 90.0;
+	dx = x - robot->_x;
+	dy = y - robot->_y;
+
+	d_e = sqrt(dx * dx + dy * dy);
+	if (dx == 0 && dy == 0)
+		desired_angle = 90;
+	else
+		desired_angle = (int)(180. / CV_PI * atan2((double)(dy), (double)(dx)));
+	theta_e = desired_angle - (int)robot->_angle;
+
+	while (theta_e > 180) theta_e -= 360;
+	while (theta_e < -180) theta_e += 360;
+
+	if (d_e > 100.)
+		Ka = 17. / 90.;
+	else if (d_e > 50)
+		Ka = 19. / 90.;
+	else if (d_e > 30)
+		Ka = 21. / 90.;
+	else if (d_e > 20)
+		Ka = 23. / 90.;
+	else
+		Ka = 25. / 90.;
+
+	if (theta_e > 95 || theta_e < -95)
+	{
+		theta_e += 180;
+
+		if (theta_e > 180)
+			theta_e -= 360;
+		if (theta_e > 80)
+			theta_e = 80;
+		if (theta_e < -80)
+			theta_e = -80;
+		if (d_e < 5.0 && abs(theta_e) < 40)
+			Ka = 0.1;
+		vr = (int)(-vc * (1.0 / (1.0 + exp(-3.0 * d_e)) - 0.3) + Ka * theta_e);
+		vl = (int)(-vc * (1.0 / (1.0 + exp(-3.0 * d_e)) - 0.3) - Ka * theta_e);
+	}
+
+	else if (theta_e < 85 && theta_e > -85)
+	{
+		if (d_e < 5.0 && abs(theta_e) < 40)
+			Ka = 0.1;
+		vr = (int)(vc * (1.0 / (1.0 + exp(-3.0 * d_e)) - 0.3) + Ka * theta_e);
+		vl = (int)(vc * (1.0 / (1.0 + exp(-3.0 * d_e)) - 0.3) - Ka * theta_e);
+	}
+
+	else
+	{
+		vr = (int)(+.17 * theta_e);
+		vl = (int)(-.17 * theta_e);
+	}
+
+	if (robot->_x < x + 10 && robot->_y < y + 10)
+	{
+		destinated = true;
+		Velocity(robot, 0, 0);
+	}// txt로 짰던 코드예외부분
+	else if (robot->_x >= width - 30 || robot->_y >= height - 30)
+	{
+		Velocity(robot, 0, 0);//일단멈춤
+							  //그 이후 어케할지는 알아서 생각 (흑흑)
+	}
+	else
+	{
+		Velocity(robot, vl, vr);//아두이노로 보내는함수!
+	}
+
+}
+
+void VisionSystem::Position_Angle(Robot *robot, double x, double y, int desired_angle)
+{
+	destinated = false;
+	Position(robot, x, y);
+	if (destinated)
+	{
+		RobotAngle(robot, desired_angle);
+	}
+}
+
+void VisionSystem::calculatingSize(double x, double y)
+{
+	//x,y 로봇현재위치값 (640, 480기준)을 width, height변수값 기준의 좌표로 만들어줌
+	double realX = width * x / 640;
+	double realY = height * y / 480;
+
+	//실제로는 이 값을  Velocity - 아두이노 보내는 부분으로 보내줘야함!
+	//아니면 Velocity - 아두이노 보내는 부분 함수 - 에서
+	//이 함수를 한 번 호출해서 값 변경덮어쓰기 한 다음 보내면 될듯
+}
+
 void VisionSystem::calculateTheLine(Mat& img_input, double x1, double y1, double x2, double y2)
 {
 	makeLine(x1, y1, x2, y2);
@@ -145,6 +278,10 @@ void VisionSystem::calculateTheLine(Mat& img_input, double x1, double y1, double
 	rtnRobotsDirection(x1, y1);
 	ver1.putstring.os_angle.name << "angle: " << angle(x1, y1, x2, y2);
 	drawLine(img_input, x1, y1, x2, y2);
+
+	r1._angle = angle(x1, y1, x2, y2);
+	r1._x = Cx;
+	r1._y = Cy;
 }
 
 void VisionSystem::drawText(Mat& img_input)
@@ -271,39 +408,65 @@ void VisionSystem::start()
 		//imshow("binary image", team.img_binary);
 		imshow("Origin IMAGE", img_input);
 
-		// User Input for N sides
-		int res = NULL;
-		while (res != 1)
-		{
-			cout << "Input Number 1 ~ 4, For Robot Move \n";
-			int res = scanf("%d", &whichPlace);
-			if (res == 0) // 성공하지 못한 입력.. 
-			{
-				cout << "Try agin \n";
-			}
-		}
-
-		// Call autoPosition for calculate angle, position
-		// send robot vr, vl
-		switch (whichPlace) {
-		case 1:
-			autoPosition(Cx, Cy, n1._x, n1._y, whichAngle);
-			break;
-		case 2:
-			autoPosition(Cx, Cy, n2._x, n2._y, whichAngle);
-			break;
-		case 3:
-			autoPosition(Cx, Cy, n3._x, n3._y, whichAngle);
-			break;
-		case 4:
-			autoPosition(Cx, Cy, n4._x, n4._y, whichAngle);
-			break;
-		}
-		
 		// Exit to ESC key
 		if (waitKey(1) == 27) {
 			serialComm.disconnect(); //작업이 끝나면 포트를 닫는다
+			cout << "종료합니다 \n";
 			break;
+		}
+		if ((char)waitKey(30) == ' ') {
+			cout << "로봇 모드 진입 \n";
+			cout << "블루투스 연결 중 ... \n";
+
+			if (!serialComm.connect("COM7")) //COM7번의 포트를 오픈한다. 실패할 경우 리턴으로 종료 한다.
+			{
+				cout << "connect faliled" << endl;
+			}
+			else {
+				cout << "connect successed" << endl;
+				while (true) {
+					cout << "전송할 (1:left, 2:right, 3:Stop, 4:Forward, 5: Back)를 입력하세요 : ";
+					cin >> buffer;
+
+					if (!serialComm.sendCommand(buffer))
+					{
+						cout << "send command failed" << endl;
+					}
+					else cout << "send Command success" << endl;
+					buffer = '\0';
+				}
+			}
+
+			// User Input for N sides
+			/*
+			int res = NULL;
+			while (res != 1)
+			{
+				cout << "Input Number 1 ~ 4, For Robot Move \n";
+				int res = scanf("%d", &whichPlace);
+				if (res == 0) // 성공하지 못한 입력.. 
+				{
+					cout << "Try agin \n";
+				}
+			}
+
+			// Call autoPosition for calculate angle, position
+			// send robot vr, vl
+			switch (whichPlace) {
+			case 1:
+				autoPosition(Cx, Cy, n1._x, n1._y, whichAngle);
+				break;
+			case 2:
+				autoPosition(Cx, Cy, n2._x, n2._y, whichAngle);
+				break;
+			case 3:
+				autoPosition(Cx, Cy, n3._x, n3._y, whichAngle);
+				break;
+			case 4:
+				autoPosition(Cx, Cy, n4._x, n4._y, whichAngle);
+				break;
+			}
+			*/
 		}
 	}
 }
@@ -311,30 +474,12 @@ void VisionSystem::start()
 int main()
 {
 	VisionSystem vs = VisionSystem();
-	/*if (!ver1.cap.isOpened())
+	if (!ver1.cap.isOpened())
 	{
 		cout << "Can not open Cam" << endl;
 		return -1;
-	}*/
-	if (!serialComm.connect("COM7")) //COM7번의 포트를 오픈한다. 실패할 경우 리턴으로 종료 한다.
-	{
-		cout << "connect faliled" << endl;
 	}
-	else {
-		cout << "connect successed" << endl;
-		while (true) {
-			cout << "전송할 (1:left, 2:right, 3:Stop, 4:Forward, 5: Back)를 입력하세요 : ";
-			cin >> buffer;
-
-			if (!serialComm.sendCommand(buffer))
-			{
-				cout << "send command failed" << endl;
-			}
-			else cout << "send Command success" << endl;
-			buffer = '\0';
-		}
-	}
-	//vs.setFourSides(); // N side
-	//vs.start();
+	vs.setFourSides(); // N side
+	vs.start();
 	return 0;
 }
